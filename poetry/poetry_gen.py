e5ffc6b100c2005cbff02aaab7131b9d7c7229fc
yuanjie <313303303@qq.com>
2019-01-08 13:12:49 +0800

    藏头诗

--- poetry/poetry_gen.py
+++ poetry/poetry_gen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-__title__ = 'main'
+__title__ = 'poetry_gen.py'
 __author__ = 'JieYuan'
 __mtime__ = '19-1-7'
 """
@@ -14,6 +14,13 @@ from .config import Config
 
 opt = Config()
 
+data, word2ix, ix2word = get_data(opt)
+model = PoetryModel(len(word2ix), 128, 256)
+# print(model)
+map_location = lambda s, l: s
+state_dict = torch.load(opt.model_path, map_location=map_location)
+model.load_state_dict(state_dict)
+
 
 class PoetryGen(object):
 
@@ -23,12 +30,6 @@ class PoetryGen(object):
     def gen(self, **kwargs):
         for i in kwargs.items():
             setattr(opt, *i)
-        data, word2ix, ix2word = get_data(opt)
-        model = PoetryModel(len(word2ix), 128, 256)
-        # print(model)
-        map_location = lambda s, l: s
-        state_dict = torch.load(opt.model_path, map_location=map_location)
-        model.load_state_dict(state_dict)
         start_words = opt.start_words.replace(',', '，').replace('.', '。').replace('?', '？').replace('!', '！')
         result = gen_acrostic(opt, model, start_words, ix2word, word2ix, opt.prefix_words)
         result = '\n'.join([result[idx: idx + 7] for idx in range(0, len(result), 7)])
@@ -40,4 +41,4 @@ class PoetryGen(object):
 
 
 if __name__ == '__main__':
-    PoetryGen.gen({'start_words': '简历投过来小米欢迎你'})
+    PoetryGen.gen({'start_words': '简历投过来'})
